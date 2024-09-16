cf1db29273602f47b889b40675db9fabed6f9947
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-09-16 05:23:42 +0000

    api go

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	response := map[string]string{"message": "hello go"}
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	response := map[string]string{"message": "hello go"}
 	json.NewEncoder(w).Encode(response)
 }
